944a6669dc3a8f74e746fc56ecf43bf25609e4a4
IMH51 <ianmartinharrison@outlook.com>
2018-12-10 14:17:02 +0000

    Set up basic routes on Sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ian"
+  end
+
+  get '/hometown' do
+    "My hometown is Rainham"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Reet Petite"
+  end
+
 end
